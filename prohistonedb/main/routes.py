a09ab3fd4cac359176b43e8b0195233927bbd5e7
Michel Olsthoorn <molsthoorn@chem.leidenuniv.nl>
2023-04-13 15:57:45 +0200

    removed comment

--- prohistonedb/main/routes.py
+++ prohistonedb/main/routes.py
@@ -19,7 +19,6 @@ from . import bp
 @bp.route("/", methods=["GET"])
 def index():
     """ Render the index page. """
-    #? Tim: What is the purpose of setting 'segment' and 'parent'? I don't see them used in your templates.
     try:
         return flask.render_template('pages/index.html.j2')
     except jinja2.TemplateNotFound:
@@ -31,14 +30,11 @@ def entry(uniprotID: str):
     """ Render the structure page for a specified entry. """
     return flask.render_template('pages/entry.html.j2', uniprotID = uniprotID)
 
-#? NOTE (Tim): I removed the superfluous '/' at the end of the URL. Let me know if you planned for multiple
-#?             about pages, then I'll give it a separate blueprint.
 @bp.route('/about')
 def about():
     """ Render the about page. """
     return flask.render_template('pages/about.html.j2')
 
-#? NOTE (Tim): I shortened the URL since we only have one categories page for now. For trailing slash see above note.
 @bp.route('/categories')
 def categories():
   return flask.render_template('pages/categories.html.j2')
