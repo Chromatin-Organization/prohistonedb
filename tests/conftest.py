2ce3669c11ab68abef2b595f4ced327b42ccfad1
Dhr. T. Jansen MSc <t.jansen@lic.leidenuniv.nl>
2023-04-13 15:28:27 +0200

    Add basic test infrastructure.

---
+++ tests/conftest.py
@@ -0,0 +1,50 @@
+""" Configuration script for pytest. """
+#***===== Imports =====***#
+#*----- PyTest -----*#
+import pytest
+
+#*----- Standard library -----*#
+import sys
+from pathlib import Path
+
+#*----- Flask & Flask Extenstions -----*#
+import flask
+from flask import Flask
+
+#*----- External packages -----*#
+
+#*----- Custom packages -----*#
+
+#*----- Local (test) imports -----*#
+
+#***===== Add & Import Main Package =====***#
+sys.path.append(str(Path(__file__).parents[1].resolve()))
+print(sys.path)
+import prohistonedb
+
+#***===== Fixtures =====***#
+#*----- App fixture -----*#
+@pytest.fixture
+def app() -> Flask:
+    """ Create an instance of the Flask app for testing. """
+    #* Preparation *#
+    app = prohistonedb.create_app(
+        test_config = {
+            "TESTING": True,
+        }
+    )
+
+    #* Yield for testing *#
+    yield app
+    #* Clean up *#
+
+#*----- Client fixture -----*#
+@pytest.fixture
+def client():
+    """ Create a client capable of making server requests for testing. """
+    return app.test_client()
+
+#*----- CLI runner fixture -----*#
+@pytest.fixture
+def runner():
+    """ Create a runner capable of executing CLI commands for testing. """
