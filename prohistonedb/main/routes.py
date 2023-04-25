fde100fbc3eb64bb72d6654392c2d088dcce0e75
Dhr. T. Jansen MSc <t.jansen@lic.leidenuniv.nl>
2023-04-25 17:48:00 +0200

    Fix Mol* Viewer. Changes in rank for entry page handled by backend for now.

--- prohistonedb/main/routes.py
+++ prohistonedb/main/routes.py
@@ -29,9 +29,15 @@ def index():
 #TODO: Input sanitization for multimer str.
 @bp.route("/entry/<uniprot_id>", methods=["GET"])
 @bp.route("/entry/<uniprot_id>/<multimer>", methods=["GET"])
-def entry(uniprot_id: str, multimer: Optional[str] = None):
+def entry(uniprot_id: str, multimer: Optional[str] = None, rank: Optional[int] = None):
     """ Render the structure page for a specified entry. """
     JSON_FIELDS = ["lineage", "lineage_json", "protein_id", "proteome_id", "gen_id", "genome_id", "ranks"]
+
+    if rank is None:
+        rank = 1
+
+    if rank < 1 or rank > 6:
+        raise ValueError(f"'{rank}' is not a valid value for the model rank.")
 
     db = database.get_db()
     query = sql.SQL(filter = sql.Filter(FieldType.UNIPROT_ID, uniprot_id))
@@ -41,7 +47,6 @@ def entry(uniprot_id: str, multimer: Optional[str] = None):
 
     for key in result.keys():
         res = result[key]
-
         
         if key in JSON_FIELDS:
             if res is None:
@@ -52,17 +57,20 @@ def entry(uniprot_id: str, multimer: Optional[str] = None):
             entry[key] = res
 
     entry["multimers"] = [multimer for multimer in entry["ranks"].keys() if entry["ranks"][multimer] != None]
-    print(entry)
 
     if multimer is None:
         multimer = entry["prefered_multimer"]
+    
+    # TODO: Better error handling
+    if multimer not in entry["multimers"]:
+        raise ValueError(f"Multimer '{multimer}' is not available for {uniprot_id}")   
 
-    path = "data/" + entry["rel_path"] + "/" + entry["uniprot_id"]
+    path = "data/" + entry["rel_path"] + "/" + uniprot_id
 
     if multimer != "monomer":
         path = path + f"_{multimer}"
         
-    return flask.render_template('pages/entry.html.j2', entry = entry, multimer = multimer, path = path)
+    return flask.render_template('pages/entry.html.j2', entry = entry, multimer = multimer, rank = rank, path = path)
 
 @bp.route('/about', methods=["GET"])
 def about():
