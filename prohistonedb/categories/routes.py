3902d1b75bc5c62a366bfac03dc3958f8e43ef0b
Dhr. T. Jansen MSc <t.jansen@lic.leidenuniv.nl>
2023-06-22 12:54:14 +0200

    Add text to all pages Add pages (with text) for individual categories. Remove "Other Categories" page.

--- prohistonedb/categories/routes.py
+++ prohistonedb/categories/routes.py
@@ -19,3 +19,8 @@ from . import bp
 def overview():
     """ Render an overview page for all the categories. """
     return flask.render_template('pages/categories.html.j2')
+
+@bp.route("/<id>", methods=["GET"])
+def with_id(id: int):
+    """ Render a page for a specific category. """
+    return flask.render_template("pages/categories.html.j2", id=id)
