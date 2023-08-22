73278f954405efd75ff4def7208bc9bbf62f1bff
Michel Olsthoorn <molsthoorn@chem.leidenuniv.nl>
2023-08-22 14:58:12 +0200

    Basket items counter functionality added

--- prohistonedb/static/assets/js/custom/matomo.js
+++ prohistonedb/static/assets/js/custom/matomo.js
@@ -3,9 +3,10 @@ var _paq = window._paq = window._paq || [];
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
 (function() {
-  var u="https://lmcat.eu/matomo/";
+  // set to "https://matomo.prohistonedb.org/" when migrated
+  var u = "https://lmcat.eu/matomo/";
   _paq.push(['setTrackerUrl', u+'matomo.php']);
-  _paq.push(['setSiteId', '4']);
+  _paq.push(['setSiteId', '4']); // set to 1 when migrated to matomo.prohistonedb.org
   var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
   g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
 })();
