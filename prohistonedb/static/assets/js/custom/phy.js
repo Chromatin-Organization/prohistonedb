0bffde6eb299b8a6b3d1fe117742bb1d7adfe934
Michel Olsthoorn <molsthoorn@chem.leidenuniv.nl>
2025-05-26 08:51:47 +0200

    Increased readabillity of Phylotree

--- prohistonedb/static/assets/js/custom/phy.js
+++ prohistonedb/static/assets/js/custom/phy.js
@@ -1,10 +1,36 @@
 // PhyD3 phylogenetic tree viewer
 const phyd3Container = document.querySelector("#phyd3-container");
 
+const opts = {
+    dynamicHide: true,
+    height: 1200,
+    invertColors: false,
+    lineupNodes: true,
+    showDomains: true,
+    showDomainNames: false,
+    showDomainColors: true,
+    showGraphs: true,
+    showGraphLegend: true,
+    showSupportValues: false,
+    maxDecimalsSupportValues: 1,
+    showLengthValues: false,
+    maxDecimalsLengthValues: 3,
+    showLength: false,
+    showNodeNames: true,
+    showNodesType: "all",
+    showPhylogram: false,
+    showTaxonomy: true,
+    showFullTaxonomy: true,
+    showSequences: false,
+    showTaxonomyColors: true,
+    backgroundColor: "#ffffff",
+    foregroundColor: "#000000",
+};
+
 function loadPhyloTree() {
     d3.xml(phyd3xml, function(xml) {
         const tree = phyd3.phyloxml.parse(xml);
-        phyd3.phylogram.build(phyd3Container, tree, {});
+        phyd3.phylogram.build(phyd3Container, tree, opts);
     });
 }
 
